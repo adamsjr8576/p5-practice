f3a070328e32698bae3a8d16e0faddbe357b5540
John Adams <adamsjr8576@gmail.com>
2020-03-31 15:15:39 -0600

    Add functionality to create ellipses around mouse movement and change color on click

--- day1/sketch.js
+++ day1/sketch.js
@@ -1,12 +1,15 @@
 function setup() {
-  createCanvas(640, 480);
+  let canvas = createCanvas(640, 480);
+  canvas.position(400, 50);
 }
 
 function draw() {
+  stroke('purple');
+  strokeWeight(3);
   if (mouseIsPressed) {
-    fill(0, 0, 255);
+    fill(0);
   } else {
-    fill(0, 255, 0);
+    fill(255);
   }
   ellipse(mouseX, mouseY, 80, 80);
 }

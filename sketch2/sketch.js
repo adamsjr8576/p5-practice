08670ee1325141af86f91d6d77924c91ff9049e5
John Adams <adamsjr8576@gmail.com>
2020-04-01 11:15:45 -0600

    Create deathly hollows insignia with circle, triangle and line

--- sketch2/sketch.js
+++ sketch2/sketch.js
@@ -4,5 +4,17 @@ function setup() {
 }
 
 function draw() {
+  background(210, 210, 210);
+  strokeWeight(5);
+  fill(210, 210, 210);
 
+  let triWidth = width/2;
+  const xCenter = width/2;
+  const yCenter = height/2;
+
+  triangle(xCenter/2, yCenter + yCenter/2, xCenter/2 + triWidth, yCenter + yCenter/2, xCenter, yCenter/2);
+
+  circle(xCenter, yCenter + yCenter/2 - triWidth/3.76, triWidth/1.88);
+
+  line(xCenter, yCenter/2, xCenter, yCenter + yCenter/2);
 }

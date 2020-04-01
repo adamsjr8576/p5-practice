046b490a67352d2273a3f798d85e24c20382a05f
John Adams <adamsjr8576@gmail.com>
2020-04-01 11:29:34 -0600

    Add click functionality to change background color

--- sketch2/sketch.js
+++ sketch2/sketch.js
@@ -3,18 +3,29 @@ function setup() {
   canvas.position(400, 50);
 }
 
+let value = 210;
+
 function draw() {
-  background(210, 210, 210);
+  background(value);
   strokeWeight(5);
-  fill(210, 210, 210);
+  fill(210);
+  strokeJoin(ROUND);
+  smooth();
 
   let triWidth = width/2;
   const xCenter = width/2;
   const yCenter = height/2;
 
-  triangle(xCenter/2, yCenter + yCenter/2, xCenter/2 + triWidth, yCenter + yCenter/2, xCenter, yCenter/2);
-
+  triangle(xCenter/2,
+    yCenter + yCenter/2,
+    xCenter/2 + triWidth,
+    yCenter + yCenter/2,
+    xCenter,
+    yCenter/2);
   circle(xCenter, yCenter + yCenter/2 - triWidth/3.76, triWidth/1.88);
-
   line(xCenter, yCenter/2, xCenter, yCenter + yCenter/2);
+}
+
+function mouseClicked() {
+  value === 210? value = 0 : value = 210;
 }

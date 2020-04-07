9dfe93372209524c6fdc38ac55f0f20759fc06ff
John Adams <adamsjr8576@gmail.com>
2020-04-07 10:01:15 -0600

    Setup sketch6

---
+++ sketch6/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  noLoop();
+}
+
+function windowResized () {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(0);
+
+}
